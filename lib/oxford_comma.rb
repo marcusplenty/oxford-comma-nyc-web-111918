4cdefa5b1170c1715f5fa50fc9635527e2eeec9f
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-17 22:16:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array.join(", ")
    
   elsif array.length > 1
-    x = array[0..array.length-1]
+    x = array[0...array.length-1]
     y = x.join(", ") + "and" + array[-1]
     return y
   end 

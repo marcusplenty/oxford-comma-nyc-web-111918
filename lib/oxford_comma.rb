0e899254840f8ec82fbd5ff25bbe068c5c16f573
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-17 22:20:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join(", ")
+  elsif array.length == 2
+    return array.join(" and ")
+   
+  elsif array.length > 2
+    x = array[0...array.length-1]
+    y = x.join(", ") + ", and " + array[-1]
+    return y
+  end 
 end
